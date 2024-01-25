7beb6ac76cbd8b2f11b9429106e87804607afa1a
p2635 <4522927+p2635@users.noreply.github.com>
2024-01-25 12:41:20 +0000

    Complete chapter 7 assertions

--- cypress/e2e/intro_to_cypress.cy.js
+++ cypress/e2e/intro_to_cypress.cy.js
@@ -13,22 +13,58 @@ describe("Introduction to Cypress", () => {
   });
 
   // 05 - Selectors
+  // Prerequisite - The board must be already created.
   it("Selectors", () => {
     cy.visit("/");
     cy.contains(".board", "new board");
     cy.get(".board");
     cy.get("#board-1");
-    cy.get("[data-cy=board-item]");
+    cy.get("[data-cy=board-item]").first();
   });
 
   // 06 - Interactions
-  it("creates a new list and a new card", () => {
+  it("creates a new list and a new card by pressing enter", () => {
     cy.visit("/board/1");
+    cy.get('[data-cy="add-list-input"]').type("New List{enter}");
+    cy.get('[data-cy="new-card"]').click();
+    cy.get('[data-cy="new-card-input"]').type("hello{enter}");
   });
 
   // 07 - Assertions
-  it("cards are visible", () => {
+  // Check bread card is visible after creating the card
+  it("create bread card and check is visible", () => {
     cy.visit("/board/1");
+
+    cy.get('[data-cy="new-card"]').click();
+    cy.get('[data-cy="new-card-input"]').type("bread{enter}");
+
+    cy.contains("bread").should("be.visible");
+  });
+
+  // 07 - Assertions
+  // Create two cards and check there are in fact two cards created
+  it("create two cards and check there are in fact two cards created", () => {
+    cy.visit("/board/1");
+
+    cy.get('[data-cy="new-card"]').click();
+    cy.get('[data-cy="new-card-input"]').type("bread{enter}");
+    cy.get('[data-cy="new-card-input"]').type("milk{enter}");
+
+    cy.get('[data-cy="card"]').should("have.length", 2);
+  });
+
+  // 07 - Assertions
+  // Create a list called 'groceries', create a card and complete it. Check the due date is green.
+  it.only("complete a task and check due date shows as completed", () => {
+    cy.visit("/board/1");
+    cy.get('[data-cy="add-list-input"]').type("groceries{enter}");
+    cy.get('[data-cy="new-card"]').click();
+    cy.get('[data-cy="new-card-input"]').type("bread{enter}");
+    cy.get('[data-cy="due-date"]').should("not.have.class", "completed");
+
+    cy.contains("bread").prev("label").children().check();
+
+    cy.get('[data-cy="due-date"]').should("have.class", "completed");
   });
 
   // 08 - Chaining and Retry
