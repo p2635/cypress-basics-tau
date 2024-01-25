921036eabd3d5694fe41eb9425e53d55aff267c0
p2635 <4522927+p2635@users.noreply.github.com>
2024-01-25 13:28:33 +0000

    Tidy extra spaces in cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,12 +5,11 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://localhost:3000",
     viewportHeight: 550,
     viewportWidth: 660,
     experimentalStudio: true,
   },
 });
 
-
-require('@applitools/eyes-cypress')(module);
+require("@applitools/eyes-cypress")(module);
