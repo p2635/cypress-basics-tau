1f8d668a10eef65cbeeb3e41e74fbdc77d994799
p2635 <4522927+p2635@users.noreply.github.com>
2024-01-26 15:52:32 +0000

    Rename spec file

--- cypress/e2e/lists.js
+++ cypress/e2e/lists.js
@@ -1,5 +1,5 @@
 // Introduction to Cypress, Chapter 6 - Interactions
-describe("Interactions", () => {
+describe("List tests", () => {
   before("Reset the data and create the board", () => {
     cy.request("POST", "/api/reset");
     cy.request("POST", "/api/boards", { name: "new board" });
